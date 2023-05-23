d52c0b1d97c2039d6528ed5859b3815c5e718b1e
Alan Andres Rodriguez <rodriguez.alanandres@gmail.com>
2023-05-23 20:11:11 -0300

    se agregaron views con handlebars. Se agregó websocket

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import apiProds from './src/routes/products.router.js';
+import handlebars from "express-handlebars";
 import apiCarts from './src/routes/carts.router.js';
+import apiProds from './src/routes/products.router.js';
 
 const app = express()
 const PORT = 8080
@@ -10,6 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products', apiProds)
 app.use('/api/carts', apiCarts)
+
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
 
 export const server = app.listen(PORT , (req, res) => {
     console.log(`Listening PORT  ${PORT}`);
---
+++ src/routes/home.routes.js
@@ -0,0 +1,21 @@
+import express from "express"
+import ProductManager from "../productManager"
+
+
+export const homeRouter = express.Router()
+
+const productManager = new ProductManager('../db/products.json')
+
+homeRouter.get('/', (req,res)=>{
+    try{
+        let allProducts = productManager.getProducts()
+
+        return res
+        .status(200)
+        .render('home', {allProducts})
+    }
+    catch (error) {
+        return res.status(500).json({ status: "error", msg: "Error" })
+    }
+    
+})
