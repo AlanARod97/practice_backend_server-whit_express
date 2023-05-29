a476cdc8be14c33a5839d0c6fedba9b2a4cd6806
Alan Andres Rodriguez <rodriguez.alanandres@gmail.com>
2023-05-29 18:04:08 -0300

    arreglo en app.js

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ const PORT = 8080
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"))
+
+
+
 app.use('/api/products', apiProds)
 app.use('/api/carts', apiCarts)
 app.use("/home",homeRouter)
