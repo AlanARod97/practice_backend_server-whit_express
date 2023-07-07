05b950d05c358ea61b27eb4cf383b030f6f9aabd
Alan Andres Rodriguez <rodriguez.alanandres@gmail.com>
2023-07-07 19:33:36 -0300

    vistas

--- src/DAO/models/users.model.js
+++ src/DAO/models/users.model.js
@@ -4,9 +4,8 @@ import { Schema, model } from "mongoose";
 export const userModel = model(
     "users",
     new Schema({
-        firstName: {type: String, required: true, max: 100},
-        lastName: {type: String, required: true, max: 100},
         email: {type: String, required: true, max: 100},
+        username: {type: String, required: true, max: 100},
         password: {type: String, required: true, max: 100},
         rol: {type: String, required: true, max: 100},
     
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,48 +1,78 @@
 import { userModel } from "../DAO/models/users.model.js";
+import { isValidPassword } from "../utils/bcrypt.js";
  class UserService{
     async getAll(){
         const users = await userModel.find(
             {},
             {
                 _id:true,
-                firstName:true,
-                lastName:true,
+                username: true,
                 email:true,
+                password: true,
+                rol: true,
 
             }
             );
         
         return users;
     }
-    async findUser({email, password}) {
+    async findUser(email, password) {
 		const user = await userModel.findOne(
 			{ email: email },
 			{
 				_id: true,
 				email: true,
-				firsName: true,
-                lastName:true,
-				password: true,
+				username: true,
+                password: true,
 				rol: true,
 			}
+
 		);
-	
-	}
+        
+        if(user && isValidPassword(password, user.password)){
+            return user;
+        }else{
+            return false
+        }
+    }
 
 
-    async create({firstName, lastName, email}){
+    async findUserByEmail(email){
+        const user = userModel.findOne(
+            {email: email},
+            {
+                _id: true,
+                email: true,
+                username: true,
+                password: true,
+                rol: true, 
+            }
+        )
+
+        return user || false; 
+    }
+
+
+    async create({email, username, password, rol}){
+        const userAlreadyExist = await this.findUserByEmail(email)
+        
+        if (userAlreadyExist){
+           return "El usuario ya ha sido registrado"
+        }
+        
         const userCreated = await userModel.create({ 
-            firstName, 
-            lastName, 
-            email 
+           email,
+           username,
+           password,
+           rol 
         });
         return userCreated;
     }
 
-    async updateOne({_id, firstName,lastName, email}){
+    async updateOne({_id, email, username, password, rol}){
         const userUptaded = await userModel.updateOne(
             { _id: _id },
-            { firstName, lastName, email }
+            { email, username, password, rol }
           );
 
           return userUptaded;
